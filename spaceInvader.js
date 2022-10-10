19c0a9a95eb5d980534b22473d9bbcb0d03c1d8a
“Tassal <tassal.hotak@student.ehb.be>
2022-10-10 15:21:19 +0200

    opdracht gemaakt

--- spaceInvader.js
+++ spaceInvader.js
@@ -14,48 +14,40 @@ function drawLine() {
     context.fillRect(300, 300, 300, 300);
 
     context.fillStyle = "#66FF66"
-    context.fillRect(300, 300, 50, 50);
+    context.fillRect(330, 400, 50, 50);
 
     context.fillStyle = "#66FF66"
-    context.fillRect(350, 350, 50, 50);
+    context.fillRect(380, 350, 50, 50);
+
+    
+    context.fillStyle = "#66FF66"
+    context.fillRect(430, 350, 50, 50);
 
     context.fillStyle = "#66FF66"
-    context.fillRect(400, 400, 50, 50);
-
-    //frgr//
+    context.fillRect(480, 350, 50, 50);
 
     context.fillStyle = "#66FF66"
-    context.fillRect(550, 300, 50, 50);
+    context.fillRect(530, 400, 50, 50);
 
     context.fillStyle = "#66FF66"
-    context.fillRect(500, 350, 50, 50);
+    context.fillRect(480, 450, 50, 50);
 
     context.fillStyle = "#66FF66"
-    context.fillRect(450, 400, 50, 50);
-
-    //mouth//
+    context.fillRect(480, 500, 50, 50);
 
     context.fillStyle = "#66FF66"
-    context.fillRect(425, 410, 50, 50);
-
-    //fefze//
+    context.fillRect(380, 500, 50, 50);
 
     context.fillStyle = "#66FF66"
-    context.fillRect(425, 550, 50, 50);
+    context.fillRect(380, 450, 50, 50);
 
-    //rgeger//
+    
+    
+   
 
-    context.fillStyle = "#66FF66"
-    context.fillRect(300, 500, 50, 50);
+    
+    
 
-    context.fillStyle = "#66FF66"
-    context.fillRect(550, 500, 50, 50);
-
-    context.fillStyle = "#66FF66"
-    context.fillRect(300, 550, 50, 50);
-
-    context.fillStyle = "#66FF66"
-    context.fillRect(550, 550, 50, 50);
     
    
 }
